0721f99e66b9924581135cfccd68f8ced7b7834e
Mykal Cuin <mykcuin@gmail.com>
2023-11-20 18:21:43 -0700

    Working through the layout for the calculator on Android.

--- androidMemoryCalculator/src/main/java/com/cuinsolutions/memorycalculator/android/MainActivity.kt
+++ androidMemoryCalculator/src/main/java/com/cuinsolutions/memorycalculator/android/MainActivity.kt
@@ -3,7 +3,12 @@ package com.cuinsolutions.memorycalculator.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -19,7 +24,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    Column(Modifier.fillMaxWidth()) {
+                        NumberEntry()
+                    }
                 }
             }
         }
@@ -27,14 +34,56 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingView(text: String) {
-    Text(text = text)
+fun NumberEntry() {
+    TextField(value = "", onValueChange = {})
+}
+
+@Composable
+fun NumberButton(number: Int) {
+    Button(onClick = { /*TODO*/ }) {
+        Text(text = number.toString())
+    }
+}
+
+@Composable
+fun NumberRow(firstNumber: Int, secondNumber: Int, thirdNumber: Int) {
+    Row {
+        NumberButton(firstNumber)
+        NumberButton(secondNumber)
+        NumberButton(thirdNumber)
+    }
+}
+
+@Composable
+fun TextButton(text: String) {
+    Button(onClick = { /*TODO*/ }) {
+        Text(text = text)
+    }
+}
+
+@Composable
+fun DeleteButton() {
+    IconButton(onClick = { /*TODO*/ }) {
+        Image(painter = , contentDescription = )
+    }
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        GreetingView("Hello, Android!")
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()) {
+                NumberEntry()
+                NumberRow(firstNumber = 7, secondNumber = 8, thirdNumber = 9)
+                NumberRow(firstNumber = 4, secondNumber = 5, thirdNumber = 6)
+                NumberRow(firstNumber = 1, secondNumber = 2, thirdNumber = 3)
+                Row(Modifier.fillMaxSize()) {
+                    NumberButton(number = 0)
+                    TextButton(text = ".")
+                }
+            }
     }
 }
